482b0fc5f05ade29b3b1b2cb0c61d804f23a316f
Konstantin Kudritskiy <konstantinkudritskiy@gmail.com>
2017-02-15 18:50:04 +0200

    '1'

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pokesort
+
+1. connect to pokemon server
+
+2. create card pokemon
--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
- header('Content-Type: application/json');
- 
- $arr = array ('text'=>'Привет,' . $_GET['name']);
- 
-    echo  json_encode($arr);
- 
-?>
